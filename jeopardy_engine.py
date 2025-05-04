ccb8b3892b8fe10caa0da2f1bca6b2010833dbdd
Madeline DeLeon <mdamusic36@gmail.com>
2025-05-04 00:51:53 -0700

    processes wiki articles VERY slowly - about 3:45 per file

--- jeopardy_engine.py
+++ jeopardy_engine.py
@@ -1,10 +1,11 @@
 import collections
+from concurrent.futures import ProcessPoolExecutor
 import math
 import os
 import re
 import spacy
 from whoosh.index import create_in
-from whoosh.fields import Schema, TEXT
+from whoosh.fields import Schema, TEXT, ID
 from whoosh.qparser import QueryParser
 
 
@@ -13,45 +14,84 @@ class JeopardyIR:
     nlp = spacy.load('en_core_web_sm')
     # english stopwords built into spaCy
     stopwords = nlp.Defaults.stop_words
+    num = 0
 
     def __init__(self, folder):
         # index made by whoosh
-        schema = Schema(title=TEXT(stored=True), body=TEXT(stored=True))
-        self.index = create_in("index", schema)
-        self.writer = self.index.writer()
+        schema = Schema(id=ID, title=TEXT(stored=True), body=TEXT)
 
+        if not os.path.exists("index"):
+            os.mkdir("index")
+
+        self.index = create_in("index", schema)
+
+        all_docs = []
         # loop through all wiki files
         for filename in os.listdir(folder):
             filepath = os.path.join(folder, filename)
+            print("reading file", filename)
             with open(filepath, "r", encoding='utf-8') as f:
-                content = f.read().split("\n\n\n")
-                # loop through each wiki article in the current file
-                for doc in content:
-                    self.clean_doc(doc)
-                    break #TODO: remove later, this is for developing
-            break #TODO: remove later, this is for developing
+                all_docs += f.read().split("[[")
+            break #TODO: remove this later
+
+        print(len(all_docs))
+
+        with self.index.writer() as writer:
+            for doc in all_docs:
+                title, body = self.clean_article(doc)
+                if title and body:
+                    writer.add_document(id=str(self.num), title=title, body=body)
+                    self.num += 1
+                 
+        print("all done :)", self.num, "articles processed")
+        
 
     """
-    Process the given plaintext of a document. Lowercase, extract the title,
-    remove punctuation/characters, remove stop words, etc. Then add the cleaned
-    information to the index. 
+    Process the given plaintext of a document. Extract the title, clean the
+    rest of the document, then add the cleaned information to the index. 
     """
-    def clean_doc(self, document):
-        # extract title (no preprocessing yet)
-        original_title = re.match(r'\[\[(.*?)\]\]', document)
+    def clean_article(self, document):
+        # extract title (with no preprocessing)
+        original_title = re.match(r'(.*?)\]\]', document)
+        body = None
         if original_title:
-            # lowercase
-            document = document.lower()
+            original_title = original_title.group().strip("[]")
+            
+            # add cleaned information to index
+            body = self.clean(document)
+        return original_title, body
+
+
+    """
+    Clean the given plaintext of a document. Lowercase, remove punctuation,
+    remove stop words, etc.
+    """
+    def clean(self, document):
+        # lowercase
+        document = document.strip().lower()
+
+        # ignore redirect articles
+        if "#redirect" not in document:
             # remove puctuation
-            document = re.sub(r'[^\w\s]', '', document)
-            ########
+            document = re.sub(r'[^\w\s]', ' ', document)
+            # remove web addresses
+            document = re.sub(r'http\S+|www.\S+', '', document)
+            # remove markdown '=='
+            document = re.sub(r'==', '', document)
+            # remove special whitespace characters
+            document = re.sub(r'[\n\t\r\f\v]+', '', document)
+            # remove extra spaces
+            document = re.sub(r"\s+", " ", document)
+            document = document.strip()
 
+            # gets the lemma of each word in the string
+            with self.nlp.select_pipes(disable=["ner", "parser"]):
+                doc = self.nlp(document)
 
-        # add cleaned information to index
-        # self.writer.add_document(title=title, body=body)
-        # self.writer.commit()  # not sure if this is needed????
-
-
+            # remove stop words and create list of tokens
+            tokens = " ".join([w.lemma_ for w in doc if len(w.lemma_.strip()) and w.lemma_ not in self.stopwords])
+            return tokens
+        return None
 
 def main():
     ir = JeopardyIR("articles")
