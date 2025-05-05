0ead5ac6ef7f38839fa741fb45fba03e1dbf76ec
Madeline DeLeon <mdamusic36@gmail.com>
2025-05-04 23:01:50 -0700

    Process wiki files into JSON

--- jeopardy_engine.py
+++ jeopardy_engine.py
@@ -1,100 +1,78 @@
-import collections
-from concurrent.futures import ProcessPoolExecutor
-import math
 import os
-import re
+import json
 import spacy
-from whoosh.index import create_in
-from whoosh.fields import Schema, TEXT, ID
-from whoosh.qparser import QueryParser
-
-
-class JeopardyIR:
-    # spaCy's natural language processing model for english
-    nlp = spacy.load('en_core_web_sm')
-    # english stopwords built into spaCy
-    stopwords = nlp.Defaults.stop_words
-    num = 0
-
-    def __init__(self, folder):
-        # index made by whoosh
-        schema = Schema(id=ID, title=TEXT(stored=True), body=TEXT)
-
-        if not os.path.exists("index"):
-            os.mkdir("index")
-
-        self.index = create_in("index", schema)
-
-        all_docs = []
-        # loop through all wiki files
-        for filename in os.listdir(folder):
-            filepath = os.path.join(folder, filename)
-            print("reading file", filename)
-            with open(filepath, "r", encoding='utf-8') as f:
-                all_docs += f.read().split("[[")
-            break #TODO: remove this later
-
-        print(len(all_docs))
-
-        with self.index.writer() as writer:
-            for doc in all_docs:
-                title, body = self.clean_article(doc)
-                if title and body:
-                    writer.add_document(id=str(self.num), title=title, body=body)
-                    self.num += 1
-                 
-        print("all done :)", self.num, "articles processed")
-        
-
-    """
-    Process the given plaintext of a document. Extract the title, clean the
-    rest of the document, then add the cleaned information to the index. 
-    """
-    def clean_article(self, document):
-        # extract title (with no preprocessing)
-        original_title = re.match(r'(.*?)\]\]', document)
-        body = None
-        if original_title:
-            original_title = original_title.group().strip("[]")
-            
-            # add cleaned information to index
-            body = self.clean(document)
-        return original_title, body
-
-
-    """
-    Clean the given plaintext of a document. Lowercase, remove punctuation,
-    remove stop words, etc.
-    """
-    def clean(self, document):
-        # lowercase
-        document = document.strip().lower()
-
-        # ignore redirect articles
-        if "#redirect" not in document:
-            # remove puctuation
-            document = re.sub(r'[^\w\s]', ' ', document)
-            # remove web addresses
-            document = re.sub(r'http\S+|www.\S+', '', document)
-            # remove markdown '=='
-            document = re.sub(r'==', '', document)
-            # remove special whitespace characters
-            document = re.sub(r'[\n\t\r\f\v]+', '', document)
-            # remove extra spaces
-            document = re.sub(r"\s+", " ", document)
-            document = document.strip()
-
-            # gets the lemma of each word in the string
-            with self.nlp.select_pipes(disable=["ner", "parser"]):
-                doc = self.nlp(document)
-
-            # remove stop words and create list of tokens
-            tokens = " ".join([w.lemma_ for w in doc if len(w.lemma_.strip()) and w.lemma_ not in self.stopwords])
-            return tokens
-        return None
 
 def main():
-    ir = JeopardyIR("articles")
+    # english natural language processor made by spaCy
+    nlp = spacy.load('en_core_web_sm', disable=["ner", "parser"])
+    
+    # A list to store all article data as dictionaries
+    articles = []
+    # current document ID
+    doc_id = 0
+    # loop through all wiki files
+    for filename in os.listdir("articles"):
+        filepath = os.path.join("articles", filename)
+        print("Reading file " + filename + "...")
+        with open(filepath, "r", encoding='utf-8') as f:
+            title = None
+            body_lines = []
 
+            # loop through each file line by line
+            for line in f:
+                line = line.strip()
 
-main()
+                if line:
+                    # if an article title is encountered
+                    if line.startswith("[[") and line.endswith("]]"):
+                        # we already have a title
+                        if title:
+                            body = " ".join(body_lines)
+                            
+                            # gets the lemma of each word
+                            body = nlp(body)
+                            # handles removing punctuation, extra spaces and stop words
+                            body = [
+                                token.lemma_.lower()
+                                for token in body
+                                if not token.is_punct and not token.is_space and not token.is_stop
+                            ]
+
+                            # add current article to list
+                            articles.append({"id": doc_id, "title": title, "body": body})
+                            doc_id += 1
+                            # reset title
+                            title = None
+                        body_lines = []
+                        title = line[2:-2]
+                    # ignore redirect articles
+                    elif "#redirect" in line.lower():
+                        title = None
+                        continue
+                    # ignore section headers
+                    elif not line.startswith('=='):
+                        body_lines.append(line)
+            # checks for edge case (last article in the file)
+            if title:
+                body = " ".join(body_lines)
+
+                # gets the lemma of each word
+                body = nlp(body)
+                # handles removing punctuation, extra spaces and stop words
+                body = [
+                    token.lemma_.lower()
+                    for token in body
+                    if not token.is_punct and not token.is_space and not token.is_stop
+                ]
+
+                articles.append({"id": doc_id, "title": title, "body": body})
+                doc_id += 1
+
+    # write all articles to an output JSON file
+    with open("cleaned_articles.json", "w", encoding='utf-8') as out_f:
+        json.dump(articles, out_f, ensure_ascii=False, indent=4)
+
+    print("All done :)", len(articles), "articles processed")
+
+# if __name__ == "__main__":
+#     main()
