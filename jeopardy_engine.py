136d055b7344c21e6b63d3faacb0026eee340976
Madeline DeLeon <mdamusic36@gmail.com>
2025-05-04 23:32:47 -0700

    basic framework

--- jeopardy_engine.py
+++ jeopardy_engine.py
@@ -2,7 +2,14 @@ import os
 import json
 import spacy
 
-def main():
+
+"""
+Go through all of the wiki files and process them into JSON. For each article,
+extract the title and convert the rest into a cleaned list of tokens.
+Args: None
+Returns: None
+"""
+def wiki_files_to_json():
     # english natural language processor made by spaCy
     nlp = spacy.load('en_core_web_sm', disable=["ner", "parser"])
     
@@ -74,5 +81,37 @@ def main():
 
     print("All done :)", len(articles), "articles processed")
 
-# if __name__ == "__main__":
-#     main()
+
+"""
+Build a TF-IDF index from all of the article data. This uses the LNC LTN
+formulas from Homework 3.
+"""
+def build_index(json_file):
+    # extract article information from JSON file (if it exists)
+    if os.path.exists(json_file):
+        with open(json_file, "r", encoding='utf-8') as f:
+            data = json.load(f)
+            print(len(data)) # should be 137,471
+    else:
+        print("ERROR: file", json_file, "does not exist. Did you unzip it?")
+
+
+def answer_questions(q_file):
+    pass
+
+
+def main():
+    #### DO NOT RUN THIS ####
+    #### This was used to create the cleaned_articles file
+    #### It only needed to be ran once
+    #wiki_files_to_json()
+
+    # Construct the tf-idf index
+    index = build_index("cleaned_articles.json")
+
+    # Process the Jeopardy questions and evaluate the results
+    answer_questions("questions.txt")
+
+
+if __name__ == "__main__":
+    main()
